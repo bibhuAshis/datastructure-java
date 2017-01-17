2318a0ecf8466c44bd88ee820093f202d2af5626
bibhuAshis <bibhuashisjena@gmail.com>
2017-01-17 14:11:33 +0530

    Front at Top

---
+++ src/testPackage/queue/FrontAtTop.java
@@ -0,0 +1,31 @@
+package testPackage.queue;
+
+
+import testPackage.stack.Stack;
+
+/**
+ * Created by bajmd on 1/16/2017.
+ */
+class QueuetoStack {
+    Stack stack = new Stack();
+    public void movetoStack(Queue queue) {
+        int temp = queue.dequeue();
+        while (queue.front <= queue.rear) {
+            stack.push(queue.dequeue());
+        }
+        stack.push(temp);
+        stack.print();
+    }
+}
+public class FrontAtTop {
+    public static void main(String rgs[]) {
+        Queue queue = new Queue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        QueuetoStack transfer = new QueuetoStack();
+        transfer.movetoStack(queue);
+    }
+}
