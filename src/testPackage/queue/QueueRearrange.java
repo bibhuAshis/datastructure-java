5355911b00d0d32dbd7c5fe951bd02d345a9e6e3
bibhuAshis <bibhuashisjena@gmail.com>
2017-01-18 15:31:30 +0530

    Rearrange Queue

---
+++ src/testPackage/queue/QueueRearrange.java
@@ -0,0 +1,33 @@
+package testPackage.queue;
+
+/**
+ * Created by bjena on 1/17/2017.
+ * Problem Statement: Given a queue of integers, rearrange the elements by interleaving the first half of the list with
+ * second half of the list.
+ */
+class Rearrange {
+    Queue tempQueue = new Queue();
+    Queue rearrangedQueue = new Queue();
+    public void rearrangeQueue(Queue queue) {
+        int m = queue.rear + 1;
+        for (int i=0; i<m / 2; i++) {
+            tempQueue.enqueue(queue.dequeue());
+        }
+        while (queue.front < m && tempQueue.front < m/2) {
+            rearrangedQueue.enqueue(tempQueue.dequeue());
+            rearrangedQueue.enqueue(queue.dequeue());
+        }
+        rearrangedQueue.print();
+    }
+}
+public class QueueRearrange {
+    public static void main (String args[]) {
+        Queue queue = new Queue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        Rearrange rearranger = new Rearrange();
+        rearranger.rearrangeQueue(queue);
+    }
+}
