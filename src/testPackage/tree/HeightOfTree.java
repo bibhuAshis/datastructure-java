9127e1235ddd14bb274f10d2084d28ac9f80c864
bibhuAshis <bibhuashisjena@gmail.com>
2017-04-18 16:10:49 -0700

    Height Alt

--- src/testPackage/tree/HeightOfTree.java
+++ src/testPackage/tree/HeightOfTree.java
@@ -17,12 +17,36 @@ public class HeightOfTree {
         return height;
     }
 
+    void findHeightAlt (TreeNode root) {
+        TreeQueue queue = new TreeQueue(10);
+        queue.enqueue(root);
+
+        int height = 0;
+
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+
+            while (count-- != 0) {
+                TreeNode temp = queue.dequeue();
+
+                if (temp.getleft() != null)
+                    queue.enqueue(temp.left);
+                if (temp.getRight() != null)
+                    queue.enqueue(temp.right);
+            }
+            height++;
+        }
+
+        System.out.println("Height of tree is " +height);
+    }
+
     public static void main (String args[]) {
         TreeNode root = new TreeNode(1);
         BinaryTree tree = new BinaryTree();
         tree.createTree(root);
 
         HeightOfTree height = new HeightOfTree();
+        height.findHeightAlt(root);
         int treeHeight = height.findHeight(root);
         System.out.println("Height of tree is " +treeHeight);
     }
